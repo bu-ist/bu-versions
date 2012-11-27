6f2dbd773a1f340243b4297bcf33d546d03f6b3b
Gregory Cornelius <gcorne@gmail.com>
2012-11-27 20:14:08 +0000

    Make sure that postL10n is defined.

--- js/bu-versions.js
+++ js/bu-versions.js
@@ -12,7 +12,7 @@ jQuery(function($) {
 	$('.bu-version-notice').show();
 	overridePublishButton();
 
-	if ( $('.bu_alt_postedit').length > 0 ) {
+	if ( $('.bu_alt_postedit').length > 0 && typeof postL10n !== 'undefined' ) {
 		// override the localization so that our text for the "Publish" button
 		// is always used.
 		postL10n['publish'] = 'Replace Original';
