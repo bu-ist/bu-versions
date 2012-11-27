e36da2085b9d5a678f8638b8ff8805de6b687619
Gregory Cornelius <gcorne@gmail.com>
2012-11-27 15:29:56 +0000

    [#40123261] Override the localization for the "Publish" button.
    This fixes an issues where javascript in WP core is changing the text of
    the submit button thus overriding the override.

--- js/bu-versions.js
+++ js/bu-versions.js
@@ -1,10 +1,20 @@
 jQuery(function($) {
+
+	var overridePublishButton = function() {
+		var $button = $('.bu_alt_postedit #publish');
+		if($button.val() == 'Publish') {
+			$button.val('Replace Original');
+		}
+	}
+
 	var notice = $('.bu-version-notice').remove();
 	$('#wpbody-content > .wrap > h2').after(notice);
 	$('.bu-version-notice').show();
+	overridePublishButton();
 
-	var $button = $('.bu_alt_postedit #publish');
-	if($button.val() == 'Publish') {
-		$button.val('Replace Original');
+	if ( $('.bu_alt_postedit').length > 0 ) {
+		// override the localization so that our text for the "Publish" button
+		// is always used.
+		postL10n['publish'] = 'Replace Original';
 	}
 });
